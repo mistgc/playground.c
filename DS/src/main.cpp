fbb9fdf6c318f411c2843bc57498356eb5b8223a
zaiic <zaiic@outlook.com>
2023-08-27 11:48:43 +0800

    build: support "pre-commit" and add the corresponding configuration

--- DS/src/main.cpp
+++ DS/src/main.cpp
@@ -5,26 +5,26 @@
 using namespace DS;
 
 TEST(DS, TestLinkStackWithoutHead) {
-    Stack<int> *s1 = new LinkStackWithoutHead<int>();
-    s1->push(1);
-    s1->push(2);
-    s1->push(3);
+  Stack<int> *s1 = new LinkStackWithoutHead<int>();
+  s1->push(1);
+  s1->push(2);
+  s1->push(3);
 
-    EXPECT_EQ(s1->size(), 3);
-    EXPECT_EQ(s1->peek(), 3);
-    EXPECT_EQ(s1->pop(), 3);
-    EXPECT_EQ(s1->pop(), 2);
+  EXPECT_EQ(s1->size(), 3);
+  EXPECT_EQ(s1->peek(), 3);
+  EXPECT_EQ(s1->pop(), 3);
+  EXPECT_EQ(s1->pop(), 2);
 
-    EXPECT_EQ(s1->size(), 1);
+  EXPECT_EQ(s1->size(), 1);
 
-    EXPECT_EQ(s1->peek(), 1);
-    EXPECT_EQ(s1->pop(), 1);
+  EXPECT_EQ(s1->peek(), 1);
+  EXPECT_EQ(s1->pop(), 1);
 
-    s1->push(100);
-    EXPECT_EQ(s1->pop(), 100);
-    s1->push(120);
-    EXPECT_EQ(s1->pop(), 120);
+  s1->push(100);
+  EXPECT_EQ(s1->pop(), 100);
+  s1->push(120);
+  EXPECT_EQ(s1->pop(), 120);
 
-    EXPECT_EQ(s1->is_empty(), true);
-    EXPECT_EQ(s1->size(), 0);
+  EXPECT_EQ(s1->is_empty(), true);
+  EXPECT_EQ(s1->size(), 0);
 }
